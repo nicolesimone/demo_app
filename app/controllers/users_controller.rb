5f2644a53a44884a5155f1e223c056b15223f7b3
Nicole February <nicole.february@gmail.com>
2014-07-03 13:38:55 +1000

    change to Users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,11 @@ class UsersController < ApplicationController
   # GET /users.json
   def index
     @users = User.all
+    
+    respond_to do |format|
+	format.html # index.html.erb
+	format.xml { render :xml => @users }
+    end
   end
 
   # GET /users/1
