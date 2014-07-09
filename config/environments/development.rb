ebf6d8e7d9f8d0d2d9997aa48bff57906bb486c3
Nicole February <nicole.february@gmail.com>
2014-07-09 11:00:07 +1000

    update dev.rb/prod.rb config.serve_static_assets = true

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,8 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  
+  # fix failing 'turbolinks' on heroku push
+  config.serve_static_assets = true
+  
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,8 @@ Rails.application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  # fix failing 'turbolinks' on heroku push - set to true
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
